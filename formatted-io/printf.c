de5485ae03e02e73af5c2610f9684e4c080d0718
ilya2s <ilyass.elouazzani97@outlook.fr>
2023-12-21 14:01:41 -0500

    Read chapter 3 and started exercices

--- formatted-io/printf.c
+++ formatted-io/printf.c
@@ -8,7 +8,7 @@ int main(void) {
     x = 839.21f;
 
     printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-    printf("|%10.3f|%10.3e|%-10g|\n",x, x, x);
-
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+    
     return 0;
 }
---
+++ formatted-io/scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int i, j;
+    float x, y;
+
+    scanf("%d%d%f%f", &i, &j, &x, &y);
+    printf("%d %d %f %f\n", i, j, x, y);
+
+    return 0;
+}
