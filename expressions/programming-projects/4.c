03e743ecae1196dde84b3e4c9ca495db320a2629
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-02-23 15:27:52 -0500

    octal conversion

---
+++ expressions/programming-projects/4.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+    int input, mod;
+    int output = 0;
+
+    printf("Enter a number between 0 and 32765: ");
+    scanf("%d", &input);
+
+    int c = 0;
+    while (input > 0) {
+        output += (input % 8) * pow(10, c);
+        input /= 8;
+        c++;
+    }
+
+    printf("In octal, your number is: %.5d\n", output);
+}
