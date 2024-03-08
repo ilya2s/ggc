8fe5207e748691cdd6db2689969d0191c8408135
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-03-07 21:21:35 -0500

    Chapter 7 started

--- lcthw/ex01.c
+++ lcthw/ex01.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+static void crash() {
+    char *test = NULL;
+    int i = 0;
+
+    for(; i < 1000000; i++) printf("%c", test[i]);
+}
+
 int main(int argc, char *argv[]) {
     int distance = 100;
 
-    printf("You are %d miles away.\n", distance);
+    crash();
+    crash();
+    crash();
+    crash();
+    crash();
+    crash();
 
     return 0;
 }
