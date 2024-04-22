f1ab1ee789a77c4f674d81a6fbac40e9820697dc
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-04-22 19:01:31 -0400

    basic types

--- lcthw/ex07.c
+++ lcthw/ex07.c
@@ -8,6 +8,8 @@ int main(int argc, char *argv[]) {
     char first_name[] = "Ilyass";
     char last_name[] = "El Ouazzani";
 
+    first_name[6] = '@';
+
     printf("You are %d miles away.\n", distance);
     printf("You have %f levels of power.\n", power);
     printf("You have %.2f awesome super powers.\n", super_power);
@@ -19,13 +21,12 @@ int main(int argc, char *argv[]) {
     int bugs = 100;
     double bug_rate = 1.2;
 
-    printf("You have %d bugs at the imaginary rate of %.1f.\n",
-                bugs, bug_rate);
+    printf("You have %d bugs at the imaginary rate of %.1f.\n", bugs, bug_rate);
 
-    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-    printf("The entire universe has: %14ld bugs.\n", universe_of_defects);
+    long universe_of_defects = 18446744073709551615Lu;
+    printf("The entire universe has: %14lu bugs.\n", universe_of_defects);
 
-    double expected_bugs = bugs * bug_rate;     // int * double -> double
+    double expected_bugs = bugs * bug_rate; // int * double -> double
     printf("You are expected to have: %9.2f bugs.\n", expected_bugs);
 
     double part_of_universe = expected_bugs / universe_of_defects;
@@ -33,9 +34,10 @@ int main(int argc, char *argv[]) {
 
     // Demo for something weird
     char nul_byte = '\0';
-    int care_percentage = bugs * nul_byte;
+    printf("\n>>> Nul byte: %s\n\n", nul_byte);
 
-    printf("Which means you should care %d%%.\n", nul_byte);
+    int care_percentage = bugs * nul_byte;
+    printf("Which means you should care %d%%.\n", care_percentage);
 
     return 0;
 }
